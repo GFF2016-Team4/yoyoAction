6b06fe056f48793c3062887bd52232420f400410
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-04-17 15:31:41 +0900

    ステージ作成用のツール追加 レールを簡単に作成できるように

---
+++ Assets/StageEditTools/Editor/RailInspector.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Rail))]
+public class RailInspector : Editor
+{
+    private Rail rail;
+
+    private void OnSceneGUI()
+    {
+        rail = target as Rail;
+
+        //線を描画
+        for (int i = 0; i < rail.points.Length - 1; i++)
+        {
+            Vector3 p0 = ShowPoint(i);
+            Vector3 p1 = ShowPoint(i+1);
+
+            Handles.color = Color.white;
+            Handles.DrawLine(p0, p1);
+        }
+    }
+
+    private Vector3 ShowPoint(int index)
+    {
+        Vector3 point = rail.points[index];
+        EditorGUI.BeginChangeCheck();
+        point = Handles.DoPositionHandle(point, Quaternion.identity);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(rail, "Move Point");
+            EditorUtility.SetDirty(rail);
+            rail.points[index] = point;
+        }
+        return point;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        rail = target as Rail;
+
+        DrawPointsInspector();
+
+        //ポイントを追加
+        if (GUILayout.Button("Add Rail"))
+        {
+            Undo.RecordObject(rail, "Add Rail");
+            rail.AddPoint();
+            EditorUtility.SetDirty(rail);
+        }
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        rail.createObject = EditorGUILayout.ObjectField ("Rail Object", rail.createObject, typeof(GameObject), false) as GameObject;
+
+        GUILayout.Label("Railの名前");
+        rail.railName = EditorGUILayout.TextField("Rail Name", rail.railName);
+
+        GUILayout.Label("GenerateはUndo出来ません");
+        if (GUILayout.Button("Generate"))
+        {
+            rail.Generate();
+        }
+    }
+
+    private void DrawPointsInspector()
+    {
+        GUILayout.Label("Selected Point");
+        GUILayout.Label("Rails Number : " + rail.points.Length);
+
+        for (int i = 0; i < rail.points.Length; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+                //削除ボタン用
+                GUILayoutOption[] option = new GUILayoutOption[]
+                {
+                    GUILayout.Width(15), 
+                    GUILayout.Height(15)
+                };
+
+                //削除ボタン
+                if (GUILayout.Button("-", option))
+                {
+                    Undo.RecordObject(rail, "Add Rail");
+                    rail.RemovePoint(i);
+                    EditorUtility.SetDirty(rail);
+                }
+
+                Vector3 pos = rail.points[i];
+                pos = EditorGUILayout.Vector3Field("Position", pos);
+                if (pos != rail.points[i])
+                {
+                    Undo.RecordObject(rail, "Move Point");
+                    EditorUtility.SetDirty(rail);
+                    rail.points[i] = pos;
+                }
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Assets/StageEditTools/Script/Rail.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class Rail : MonoBehaviour
+{
+    public Vector3[] points;
+    public string    railName = "Rail";
+
+    public GameObject createObject;
+
+    void Reset()
+    {
+        points = new Vector3[] {
+            new Vector3(1f, 0f, 0f),
+            new Vector3(2f, 0f, 0f),
+            new Vector3(3f, 0f, 0f)
+        };
+    }
+
+    public void AddPoint()
+    {
+        Vector3 point = points[points.Length - 1];
+        Array.Resize(ref points, points.Length + 1);
+        point.x += 1f;
+        points[points.Length - 1] = point;
+    }
+
+    public void RemovePoint(int index)
+    {
+        List<Vector3> list = new List<Vector3>(points);
+        list.RemoveAt(index);
+        points = list.ToArray();
+    }
+
+    public void Generate()
+    {
+        if (createObject == null)
+        {
+            Debug.LogError("エラー : プレハブが設定されていません");
+            return;
+        }
+
+        GameObject railParent = new GameObject();
+        railParent.name = railName;
+
+        for (int i = 0; i < points.Length-1; i++)
+        {
+            Vector3 p1 = points[i + 0];
+            Vector3 p2 = points[i + 1];
+
+            //中点
+            Vector3    midPoint = CalcMidPoint(p1, p2);
+            float      distance = Vector3.Distance(p1, p2);
+            Quaternion quat     = Quaternion.LookRotation(CalcDirection(p1, p2));
+
+            //生成
+            GameObject obj = Instantiate(createObject);
+
+            //初期化
+            obj.transform.position = midPoint;
+            obj.transform.rotation = quat;
+
+            Vector3 scale = obj.transform.localScale;
+            scale.z  = distance;
+            
+            obj.transform.localScale = scale;
+
+            obj.transform.parent = railParent.transform;
+        }
+    }
+    
+    Vector3 CalcMidPoint(Vector3 vec1, Vector3 vec2)
+    {
+        return (vec1 + vec2) / 2.0f;
+    }
+
+    Vector3 CalcDirection(Vector3 vec1, Vector3 vec2)
+    {
+        return vec2 - vec1;
+    }
+}
