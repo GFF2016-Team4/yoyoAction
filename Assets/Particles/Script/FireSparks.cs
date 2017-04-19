6176d0ab1ea45830cbf135e9a5652f6bbf3dfff8
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-04-19 14:21:45 +0900

    パーティクル追加 : Fire Spark(レールを滑っている時などに)

---
+++ Assets/Particles/Script/FireSparks.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireSparks : MonoBehaviour
+{
+    [Header("大きいほど一度にでるパーティクルの量が変わる")]
+    public float rate;
+    public float randomRange;
+
+    new ParticleSystem            particleSystem;
+    ParticleSystem.EmissionModule module;
+    Vector3 prevPos;
+
+    void Awake()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        module = particleSystem.emission;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float speed = Vector3.Distance(transform.position, prevPos);
+
+        module.rateOverDistance = Random.Range(speed*rate - randomRange, speed*rate + randomRange); 
+        prevPos = transform.position;
+    }
+}
