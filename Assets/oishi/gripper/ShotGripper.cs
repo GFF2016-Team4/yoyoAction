3aebf67fdddd2436f16a6c82c0779f1074349329
chulos <latale_kaeru@yahoo.co.jp>
2017-05-02 17:40:00 +0900

    射出/巻き取り

---
+++ Assets/oishi/gripper/ShotGripper.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotGripper : MonoBehaviour
+{
+    ////球rayの半径
+    //public float radius;
+    ////球rayの飛ばす距離
+    //public float direction;
+
+    //private Vector3 position;
+    //private float distance = -30.0f;
+    RopeSimulate ropeSimulate;
+    Player player;
+
+    Vector3 movePos;
+    Vector3 point;
+
+    public GameObject Rope;
+    public GameObject CopyRope = null;
+    private GameObject target;
+
+    bool IsBullet = false;
+    bool IsIK = false;
+
+    void Awake()
+    {
+        target = GameObject.Find("Player");
+        player = target.GetComponent<Player>();
+        point = player.HitPoint;
+    }
+    void Start()
+    {
+        //生成した時にプレイヤーと接触して微妙に位置がずれるため
+        //最初はオフ
+        GetComponent<SphereCollider>().enabled = false;
+        //弾を飛ばす
+        ShotBullet();
+        //ropeSimulate.SimulationStop();
+    }
+
+    void Update()
+    {
+        //ロープの発射
+        if (IsBullet == false)
+        {
+            ropeSimulate.originPosition = transform.position;
+        }
+        //物理挙動をしている時はプレイヤーとロープの末尾を同期
+        if (IsBullet == true && IsIK == true)
+        {
+            player.transform.position = ropeSimulate.tailPosition;
+        }
+        //物理挙動をシていない時はロープの末尾とプレイヤーを同期
+        else ropeSimulate.tailPosition = getPlayerPosition;
+
+        //ロープの挙動on
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            IsIK = true;
+            ropeSimulate.SimulationStart();
+        }
+        //ロープの巻き取り(ロープが移動)
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            MoveToPlayerPosition_(transform.position);
+        }
+        //ロープの巻き取り(プレイヤーが移動)
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            MoveToOriginRope_(getPlayerPosition);
+        }
+    }
+    public void ShotBullet()
+    {
+        CopyRope = Instantiate(Rope, transform.position, Quaternion.identity);
+
+        ropeSimulate = CopyRope.GetComponent<RopeSimulate>();
+        CopyRope.transform.parent = transform;
+
+        //初期化           引数(origin,tail) 
+        ropeSimulate.InitPosition(transform.position, getPlayerPosition);
+        //最初は物理挙動off
+        ropeSimulate.SimulationStop();
+
+        MoveToTarget_(transform.position, point);
+    }
+
+    //------------------------同じような事書いてるので要修正-------------------------------
+    //OriginRope→target
+    Coroutine MoveToTarget_(Vector3 current, Vector3 target)
+    {
+        return StartCoroutine(MoveToTarget(current, target));
+    }
+
+    //現在地、目的地
+    IEnumerator MoveToTarget(Vector3 current, Vector3 target)
+    {
+        for (float i = 0.0f; i < 1f; i += Time.deltaTime)
+        {
+            float t = i / 1f;
+
+            movePos = target;
+            transform.position = Vector3.Lerp(current, movePos, t);
+            ropeSimulate.originPosition = transform.position;
+
+            yield return null;
+        }
+        transform.position = movePos;
+
+        //移動が終わったらコライダーを戻す
+        //GetComponent<SphereCollider>().enabled = true;
+
+        IsBullet = true;
+    }
+
+    //OriginRope→Player
+    Coroutine MoveToPlayerPosition_(Vector3 current)
+    {
+        return StartCoroutine(MoveToPlayerPosition(current));
+    }
+
+    //現在地
+    IEnumerator MoveToPlayerPosition(Vector3 current)
+    {
+        //プレイヤーと接触して位置がずれるためoffにする
+        GetComponent<SphereCollider>().enabled = false;
+        for (float i = 0.0f; i < 1f; i += Time.deltaTime)
+        {
+            float t = i / 1f;
+
+            transform.position = Vector3.Lerp(current, getPlayerPosition, t);
+            ropeSimulate.originPosition = transform.position;
+
+            yield return null;
+        }
+        transform.position = getPlayerPosition;
+        IsBullet = true;
+
+        Destroy(gameObject);
+    }
+
+    //Player→OriginRope
+    Coroutine MoveToOriginRope_(Vector3 current)
+    {
+        return StartCoroutine(MoveToOriginRope(current));
+    }
+
+    //現在地
+    IEnumerator MoveToOriginRope(Vector3 current)
+    {
+        //プレイヤーと接触して位置がずれるためoffにする
+        GetComponent<SphereCollider>().enabled = false;
+        Vector3 pf = player.transform.forward.normalized;
+
+        for (float i = 0.0f; i < 1f; i += Time.deltaTime)
+        {
+            float t = i / 1f;
+            //                                                                     プレイヤーの半径分マイナス
+            player.transform.position = Vector3.Lerp(current, ropeSimulate.originPosition - pf * 0.5f, t);
+
+            //player.transform.position = Vector3.Lerp(getPlayerPosition, ropeSimulate.originPosition, t);
+
+            yield return null;
+        }
+
+        player.transform.position = ropeSimulate.originPosition - pf * 0.5f;
+        IsBullet = true;
+
+    }
+
+    //------------------------同じような事書いてるので要修正-------------------------------
+
+    Vector3 getPlayerPosition
+    {
+        get { return player.Position; }
+    }
+}
--- Assets/oishi/player/script/player.cs
+++ Assets/oishi/player/script/player.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using System;
 using UniRx;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    CharacterController m_chara;
-    RopeSimulate ropeSimulate;
+    CharacterController characterController;
     checkGround check;
 
     [Header("移動速度")]
@@ -23,136 +23,128 @@ public class player : MonoBehaviour
     public float gravity;
     [Header("ジャンプ慣性の力")]
     public float inertia;
+    [Header("ロープを伸ばせる距離")]
+    public float distance;
 
     public Camera CameraBox;
-    public GameObject Rope;
-    private GameObject CopyRope = null;
-    private GameObject originRope = null;
-    private GameObject tailRope = null;
-    [Header("(テスト用)ロープ先端の位置")]
-    public GameObject target;
+    public Camera Pcamera;
+    public GameObject Bullet;
+    private GameObject CopyBullet = null;
 
     Vector3 moveDirection = Vector3.zero;
     //Vector3 NormalizeDirection;
-    //Vector3 Center;
+    Vector3 Center;
     Vector3 Accel;
+    Vector3 movePos;
     //Vector3 correction;
     RaycastHit hit;
+    public RaycastHit hitShot;
     bool isJump;
     float timer;
+    int layerMask;
+
 
     void Start()
     {
-        m_chara = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
         check = GetComponent<checkGround>();
         jumpTemp = jumpPower;
         speedTemp = speed;
         isJump = false;
-        //Center = new Vector3(Screen.width / 2, Screen.height / 2, 0); 
+        Center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        layerMask = LayerMask.GetMask("Player");
+        layerMask = ~layerMask;
+
         // correction = new Vector3(0, direction, 0);
 
     }
     void Update()
     {
-
-        if (CopyRope == null)
+        //地面に接している時
+        if (check.IsGrounded)
         {
-            //地面に接している時
-            if (check.IsGrounded)
+            isJump = false;
+            jumpTimer = 0.0f;
+            //カメラの向きに移動
+            float dx = Input.GetAxis("Horizontal");
+            float dz = Input.GetAxis("Vertical");
+            moveDirection = Quaternion.Euler(0, CameraBox.transform.localEulerAngles.y, 0) *
+                            new Vector3(dx, uPower, dz);
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            //向いてる方向ベクトルの正規化
+            //NormalizeDirection = moveDirection.normalized;
+
+            //移動速度処理
+            //加速力ある時は加速
+            moveDirection *= speed;
+
+            //坂道判定
+            if (Physics.Raycast(transform.position, Vector3.down, out hit))
             {
-                isJump = false;
-                jumpTimer = 0.0f;
-                //カメラの向きに移動
-                float dx = Input.GetAxis("Horizontal");
-                float dz = Input.GetAxis("Vertical");
-                moveDirection = Quaternion.Euler(0, CameraBox.transform.localEulerAngles.y, 0) *
-                                new Vector3(dx, uPower, dz);
-                moveDirection = transform.TransformDirection(moveDirection);
+                //坂の角度
+                //float angle = Vector3.Angle(hit.normal, Vector3.up);
+                Accel = Vector3.ProjectOnPlane(moveDirection.normalized, hit.normal);
 
-                //向いてる方向ベクトルの正規化
-                //NormalizeDirection = moveDirection.normalized;
-
-                //移動速度処理
-                //加速力ある時は加速
-                moveDirection *= speed;
-
-                //坂道判定
-                if (Physics.Raycast(transform.position, Vector3.down, out hit))
+                //坂道での速度処理
+                //下り坂 + 移動入力有(加速)
+                if (Accel.y <= -0.1f && (dx != 0 || dz != 0))
                 {
-                    //坂の角度
-                    //float angle = Vector3.Angle(hit.normal, Vector3.up);
-                    Accel = Vector3.ProjectOnPlane(moveDirection.normalized, hit.normal);
-
-                    //坂道での速度処理
-                    //下り坂 + 移動入力有(加速)
-                    if (Accel.y <= -0.1f && (dx != 0 || dz != 0))
-                    {
-                        AccelAdd(2);
-                    }
-                    //上り坂 or 平面 + 移動入力有
-                    else if (speed >= speedTemp && (dx != 0 || dz != 0))
-                    {
-                        speed -= (Time.deltaTime * 5);
-                    }
-                    //上り坂 or 平面 + 移動入力無
-                    else if (speed >= speedTemp && (dx == 0 && dz == 0))
-                    {
-                        speed = speedTemp;
-                    }
+                    AccelAdd(2);
                 }
-
-            }
-            //スペースキーでジャンプ
-            if (Input.GetButton("Jump") && isJump == false && jumpTime >= jumpTimer)
-            {
-                jumpPower += Time.deltaTime;
-                jumpTimer += Time.deltaTime;
-                moveDirection.y = jumpPower;
-
-                if (jumpTime <= jumpTimer)
+                //上り坂 or 平面 + 移動入力有
+                else if (speed >= speedTemp && (dx != 0 || dz != 0))
                 {
-                    isJump = true;
-                    jumpPower = jumpTemp;
-                    moveDirection.y *= inertia;
+                    speed -= (Time.deltaTime * 5);
+                }
+                //上り坂 or 平面 + 移動入力無
+                else if (speed >= speedTemp && (dx == 0 && dz == 0))
+                {
+                    speed = speedTemp;
                 }
             }
-            //スペースキーを離す、上昇中
-            if (Input.GetButtonUp("Jump") && jumpTime >= jumpTimer && moveDirection.y >= 0.0f)
+
+        }
+        //スペースキーでジャンプ
+        if (Input.GetButton("Jump") && isJump == false && jumpTime >= jumpTimer)
+        {
+            jumpPower += Time.deltaTime;
+            jumpTimer += Time.deltaTime;
+            moveDirection.y = jumpPower;
+
+            if (jumpTime <= jumpTimer)
             {
                 isJump = true;
                 jumpPower = jumpTemp;
                 moveDirection.y *= inertia;
             }
-
-            moveDirection.y -= gravity * Time.deltaTime;
-            m_chara.Move(moveDirection * Time.deltaTime);
-
-            //テスト用
-            //targetオブジェクトと自身の位置でロープを生成
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                CopyRope = Instantiate(Rope, target.transform.position, Quaternion.identity);
-                ropeSimulate = CopyRope.GetComponent<RopeSimulate>();
-
-                //初期化           引数(origin,tail) 
-                ropeSimulate.Initialize(target.transform.position, transform.position);
-                //ropeSimulate.SimulationStop();
-            }
         }
-        else
+        //スペースキーを離す、上昇中
+        if (Input.GetButtonUp("Jump") && jumpTime >= jumpTimer && moveDirection.y >= 0.0f)
         {
-            //ロープ削除
-            if (Input.GetKeyDown(KeyCode.J))
+            isJump = true;
+            jumpPower = jumpTemp;
+            moveDirection.y *= inertia;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+
+
+        //グリッパーを射出してない場合
+        if (CopyBullet == null)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Center);
+            var isHit = Physics.Raycast(ray, out hitShot, distance, layerMask);
+            if (isHit)
             {
-                Destroy(CopyRope);
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    //弾の生成
+                    CopyBullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+                }
             }
-
-            //ロープの基準点とtargetを同期
-            //ropeSimulate.originPosition = target.transform.position;
-            CopyRope.transform.position = target.transform.position;
-
-            //ロープの末尾と位置を同期
-            transform.position = ropeSimulate.tailPosition;
+            Debug.DrawRay(ray.origin, Camera.main.transform.forward * 100, Color.red);
         }
     }
 
@@ -161,4 +153,28 @@ public class player : MonoBehaviour
     {
         speed += (value * Time.deltaTime * 3);
     }
+    //public bool IsShpereHit()
+    //{
+    //    Ray ray = Camera.main.ScreenPointToRay(Center);
+
+    //    //基点、半径、方向、hitした情報、距離、例外判定
+    //    var sphereHit = (Physics.SphereCast(transform.position, 2, Pcamera.transform.forward, out hit, Mathf.Infinity, layerMask));
+
+    //    if (sphereHit) return true;
+    //    else return false;
+    //}
+
+    public Vector3 HitPoint
+    {
+        get { return hitShot.point; }
+    }
+    public Vector3 HitPosition
+    {
+        get { return hitShot.transform.position; }
+    }
+    public Vector3 Position
+    {
+        get { return transform.position; }
+    }
+
 }
