a3ebe3fd991a8e7b0802924b3466d6d1f2960a10
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-05-26 16:15:53 +0900

    カウントダウンのシーンを追加

---
+++ Assets/Gimmick/Scripts/StageCountdown.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StageCountdown : MonoBehaviour
+{
+    [SerializeField]
+    Text text;
+
+    [SerializeField]
+    UnityEvent unityEvent;
+
+    WaitForSeconds second = new WaitForSeconds(1.0f);
+
+    IEnumerator Start()
+    {
+        text.text = "3";
+        Debug.Log("3");
+        yield return second;
+
+        text.text = "2";
+        Debug.Log("2");
+        yield return second;
+
+        text.text = "1";
+        Debug.Log("1");
+        yield return second;
+
+        text.text = "GO";
+        Debug.Log("0");
+        yield return second;
+
+        unityEvent.Invoke();
+    }
+}
