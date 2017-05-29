d7c55b93c6700cce51a6afcffcd64620f5c7f877
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-05-29 17:03:11 +0900

    カウントダウンの終了通知

---
+++ Assets/Gimmick/Scripts/CallStageCountdown.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CallStageCountdown : MonoBehaviour
+{
+    const string countdownScene = "CountDown";
+
+    public bool isFinish { get; private set; } = false;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        SceneManager.LoadScene(countdownScene, LoadSceneMode.Additive);
+
+        yield return StartCoroutine(WaitForUnloadScene());
+
+        isFinish = true;
+    }
+
+    IEnumerator WaitForUnloadScene()
+    {
+        while (SceneManager.sceneCount > 1)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Gimmick/Scripts/SceneUnloader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneUnloader : MonoBehaviour
+{
+    //このコンポーネントがあるシーンを削除
+    //UnityActionに登録するためにMonoBehaviour継承
+    public void UnloadScene()
+    {
+        Scene unloadScene = gameObject.scene;
+        SceneManager.UnloadSceneAsync(unloadScene);
+    }
+}
