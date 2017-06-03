023b332beaa732c208bd1bf4c73bd622174dc329
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-06-04 00:22:43 +0900

    落下（？）オブジェクト仮実装

---
+++ Assets/Gimmick/Scripts/FallingObject.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Renderer))]
+public class FallingObject : MonoBehaviour
+{
+    public GameObject particle;
+
+    public float speed;
+    Material mat;
+    
+    Color matColor;
+
+    void Awake()
+    {
+        var renderer = GetComponent<Renderer>();
+        mat = renderer.materials[0];
+        mat.EnableKeyword("_EMISSION");
+        matColor = mat.GetColor("_EmissionColor");
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Flash());
+        }
+    }
+
+    IEnumerator Flash()
+    {
+        for (float i = 0.2f; i < 1.0; i+=Time.deltaTime*speed)
+        {
+            mat.SetColor("_EmissionColor", matColor * i * 5);
+            yield return null;
+        }
+        mat.SetColor("_EmissionColor", matColor * 5);
+
+        Instantiate(particle, transform.position, Quaternion.Euler(-90, 0, 0));
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Gimmick/Scripts/ParticleAutoDestory.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParticleAutoDestory : MonoBehaviour
+{
+    public float destoryTime;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(destoryTime);
+        Destroy(gameObject);
+    }
+}
