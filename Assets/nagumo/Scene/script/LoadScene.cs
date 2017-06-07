1412e6bf7319c5528ae86624acc125d00e1bcb16
unknown <>
2017-06-07 18:19:24 +0900

    Scene遷移

---
+++ Assets/nagumo/Scene/script/LoadScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour {
+    public float speed;
+    public List<Graphic> fadeUIList;
+
+    void SetAlphaUI(float t)
+    {
+        foreach(var ui in fadeUIList)
+        {
+            Color alpha = ui.color;
+            alpha.a = t;
+            ui.color = alpha;
+        }
+    }
+
+	// Use this for initialization
+	IEnumerator Start () {
+		for(float t = 0; t < 1.0f; t += Time.deltaTime * speed)
+        {
+            SetAlphaUI(t);
+            yield return null;
+        }
+        SetAlphaUI(1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
