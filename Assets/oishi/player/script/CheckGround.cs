6889f46b587cab5e22b03ad39c4e6edf3de97fa0
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-06-08 18:52:46 +0900

    バックアップ

--- Assets/oishi/player/script/CheckGround.cs
+++ Assets/oishi/player/script/CheckGround.cs
@@ -5,14 +5,32 @@ using UniRx;
 
 public class CheckGround : MonoBehaviour
 {
-    [Header("isGround判定の変更を何フレーム固定するか")]
+	public enum SlopeState
+	{
+		Flatten,
+		DownHill,
+		UpHill,
+	}
+
+	[Header("isGround判定の変更を何フレーム固定するか")]
     public int rockFrameTime = 5;
 
     private bool _isGrounded;
     public bool IsGrounded { get { return _isGrounded; } }
 
-    // Use this for initialization
-    void Start()
+	Vector3 down = Vector3.down;
+
+	RaycastHit groundHit;
+	public GameObject groundInfo
+	{
+		get
+		{
+			return groundHit.collider.gameObject;
+		}
+	}
+
+	// Use this for initialization
+	void Start()
     {
         //isGroundの値が変化してからrockFrameTime以内の変更を無視
         var check = GetComponent<CharacterController>();
@@ -24,4 +42,21 @@ public class CheckGround : MonoBehaviour
                    _isGrounded = x;
                });
     }
+
+	void Update()
+	{
+		//地面の情報を更新
+		Physics.Raycast(transform.position, down, out groundHit);
+	}
+
+	///<summary>指定した座標のスロープの情報を取得します</summary>
+	public SlopeState FetchSlope(Vector3 origin, Vector3 moveDirection)
+	{
+		Vector3 groundAngle = Vector3.ProjectOnPlane(moveDirection.normalized, groundHit.normal);
+
+		if (groundAngle.y >= 0.1f) { return SlopeState.UpHill;   } //上り坂
+		if (groundAngle.y <= 0.1f) { return SlopeState.DownHill; } //下り坂
+
+		return SlopeState.Flatten;                                  //平面
+	}
 }
