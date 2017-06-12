d3ce5b4f12bfbcf11678d58bdd82cb27106c2fb2
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-06-12 17:16:43 +0900

    Vector3.IsZero関数を追加 ゼロベクトルかどうかを調べます

---
+++ Assets/ScriptExtension/Vector3Extension.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Vector3Extension
+{
+	public static bool IsZero(this Vector3 self)
+	{
+		return self.x == 0.0f &&
+			   self.y == 0.0f &&
+			   self.z == 0.0f;
+	}
+}
