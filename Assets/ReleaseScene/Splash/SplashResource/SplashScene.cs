90caae6ebc87b452c76fb50512f84f1d4b3c868d
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-06-12 17:20:15 +0900

    Splashシーン追加

---
+++ Assets/ReleaseScene/Splash/SplashResource/SplashScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SplashScene : MonoBehaviour
+{
+    public float showTime = 3;
+
+    public string TitleName;
+
+    public FadeManager manager;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(showTime);
+
+        //シーン遷移
+        manager.SceneChange(TitleName);
+    }
+}
